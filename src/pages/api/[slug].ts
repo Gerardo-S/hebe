750eb76ee6f13ef7e1237f49aed1b1af9c85c4e2
Gerardo Solis <gerardoa.solis@yahoo.com>
2022-08-23 19:38:23 -0700

    Init boilerplate w/prisma and planetscale schema, set up query param
     TODO: Fix redirect for short link

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+
+const link = async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"];
+
+  console.log("type", slug);
+  // type of string[] not allowed
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
+
+    res.send(JSON.stringify({ message: "please use with a slug" }));
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: { slug: { equals: slug } },
+  });
+  if (!data) {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "slug not found" }));
+
+    return;
+  }
+
+  return res.redirect(data.url);
+};
+
+export default link;
